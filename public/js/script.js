59db59766789f0b088b211f62a68f0ceb1ae06bc
Luis Lithgow <lalithgow@gmail.com>
2016-07-01 12:41:10 -0400

    Added atr to the dynamically created li's.

--- public/js/script.js
+++ public/js/script.js
@@ -6,83 +6,154 @@ $(document).ready(function() {
       method: 'get',
       dataType: 'json',
       success: function(data) {
+
         data.forEach(function(trail) {
           // console.log(data);
-          let dropdown = $(this);
 
           if (trail.Prop_ID.charAt(0) === "X"  ) {
-          let $li=  $("<li>").text(trail.Park_Name + " a.k.a. " + trail.Name).addClass('li-color');
+          let $li=  $("<li>").text(trail.Park_Name + " a.k.a. " + trail.Name)
+                .addClass('li-color').attr({"data-park-name":trail.Park_Name,"data-trail-name":trail.Name, "data-accessible":trail.Accessible, "data-length":trail.Length, "data-location":trail.Location, "data-difficulty":trail.Difficulty, "data-other-details":trail.Other_Details});
           let $ul = $(".bronx-locat")
           console.log(trail.Park_Name);
           $ul.append($li);
-
-          // let $this = dropdown.val();
-          $('.bronx-locat li').off().on('click',function(event) {
-            // event.preventDefault();
-            // let choose= $(this).val();
-            // let $li=  $("<li>").text(trail.Park_Name + " a.k.a. " + trail.Name).addClass('li-color');
-            // let rightDiv = $('.right-align');
-            // rightDiv.append($li);
-            console.log($(this).text());
-          })
           }
-            // if (trail.Prop_ID.charAt(0) === "R" ) {
-            //   let $li=  $("<li>").text(trail.Park_Name +" a.k.a. " +trail.Name ).addClass('li-color');
-            //   let $ul = $(".si-locat")
-            //   console.log(trail.Park_Name);
-            //   $ul.append($li);
+          if (trail.Prop_ID.charAt(0) === "R" ) {
+              let $li=  $("<li>").text(trail.Park_Name + " a.k.a. " + trail.Name)
+                    .addClass('li-color').attr({"data-park-name":trail.Park_Name,"data-trail-name":trail.Name, "data-accessible":trail.Accessible, "data-length":trail.Length, "data-location":trail.Location, "data-difficulty":trail.Difficulty, "data-other-details":trail.Other_Details});
+              let $ul = $(".si-locat");
+              console.log(trail.Park_Name);
+              $ul.append($li);
 
-            //   // let $(this) = dropdown.val();
-            //   $(this).on('click',function() {
-            //   let $li=  $("<li>").text(trail.Park_Name + " a.k.a. " + trail.Name).addClass('li-color');
-            //   let rightDiv = $('.right-align');
-            //   rightDiv.append($li);
-            // })
-            // }
-            //  if (trail.Prop_ID.charAt(0) === "Q" ){
-            //   let $li=  $("<li>").text(trail.Park_Name +" a.k.a. " +trail.Name ).addClass('li-color');
-            //   let $ul = $(".queens-locat");
-            //   console.log(trail.Park_Name);
-            //   $ul.append($li);
+            }
+            if (trail.Prop_ID.charAt(0) === "Q" ){
+                let $li=  $("<li>").text(trail.Park_Name + " a.k.a. " + trail.Name)
+                    .addClass('li-color').attr({"data-park-name":trail.Park_Name,"data-trail-name":trail.Name, "data-accessible":trail.Accessible, "data-length":trail.Length, "data-location":trail.Location, "data-difficulty":trail.Difficulty, "data-other-details":trail.Other_Details});
+              let $ul = $(".queens-locat");
+               console.log(trail.Park_Name);
+               $ul.append($li);
 
-            //   // let $(this) = dropdown.val();
-            //   $(this).on('click',function() {
-            //   let $li=  $("<li>").text(trail.Park_Name + " a.k.a. " + trail.Name).addClass('li-color');
-            //   let rightDiv = $('.right-align');
-            //   rightDiv.append($li);
-            // })
-            // }
-            //  if (trail.Prop_ID.charAt(0) === "M" ) {
-            //   let $li=  $("<li>").text(trail.Park_Name +" a.k.a. " +trail.Name ).addClass('li-color');
-            //   let $ul = $(".manh-locat");
-            //   console.log(trail.Park_Name);
-            //   $ul.append($li);
+            }
+            if (trail.Prop_ID.charAt(0) === "M" ) {
+                let $li=  $("<li>").text(trail.Park_Name + " a.k.a. " + trail.Name)
+                    .addClass('li-color').attr({"data-park-name":trail.Park_Name,"data-trail-name":trail.Name, "data-accessible":trail.Accessible, "data-length":trail.Length, "data-location":trail.Location, "data-difficulty":trail.Difficulty, "data-other-details":trail.Other_Details});
+                let $ul = $(".manh-locat");
+                console.log(trail.Park_Name);
+                $ul.append($li);
 
-            //   // let $(this) = dropdown.val();
-            //   $(this).on('click',function() {
-            //   let $li=  $("<li>").text(trail.Park_Name + " a.k.a. " + trail.Name).addClass('li-color');
-            //   let rightDiv = $('.right-align');
-            //   rightDiv.append($li);
-            // })
-            // }
-            //   if (trail.Prop_ID.charAt(0) === "B" ) {
-            //   let $li=  $("<li>").text(trail.Park_Name +" a.k.a. " +trail.Name).addClass('li-color');
-            //   let $ul = $(".brook-locat")
-            //   console.log(trail.Park_Name);
-            //   $ul.append($li);
-
-            //   // let $(this) = dropdown.val();
-            //   $(this).on('click',function() {
-            //   let $li=  $("<li>").text(trail.Park_Name + " a.k.a. " + trail.Name).addClass('li-color');
-            //   let rightDiv = $('.right-align');
-            //   rightDiv.append($li);
-            // })
-            // }
+            }
+            if (trail.Prop_ID.charAt(0) === "B" ) {
+              let $li=  $("<li>").text(trail.Park_Name + " a.k.a. " + trail.Name)
+                    .addClass('li-color').attr({"data-park-name":trail.Park_Name,"data-trail-name":trail.Name, "data-accessible":trail.Accessible, "data-length":trail.Length, "data-location":trail.Location, "data-difficulty":trail.Difficulty, "data-other-details":trail.Other_Details});
+                let $ul = $(".brook-locat");
+                console.log(trail.Park_Name);
+                $ul.append($li);
+            }
         });
+
+          // ADDING EVENT LISTENER TO LI TAGS
+          $('.bronx-locat li').off().on('click',function(event) {
+          // event.preventDefault();
+          const $trail = $(this);
+          let $parkName=  $("<li>").text("Park: " + $trail.attr('data-park-name')).addClass('li-color');
+          let $trailName=  $("<li>").text("Trail Name: " + $trail.attr('data-trail-name')).addClass('li-color');
+          let $location=  $("<li>").text("Location: " + $trail.attr('data-location')).addClass('li-color');
+          let $accessible=  $("<li>").text("Accessible: " + $trail.attr('data-accessible')).addClass('li-color');
+          let $length=  $("<li>").text("Length: " + $trail.attr('data-length')).addClass('li-color');
+          let $difficulty=  $("<li>").text("Difficulty: " +$trail.attr('data-difficulty')).addClass('li-color');
+          let $otherDets = $("<li>").text("Details " + $trail.attr('data-other-details') ).addClass('li-color');
+
+          // APPENDING
+          let rightDiv = $('.right-align');
+          // let $ul = $("<ul>");
+          rightDiv.append($parkName, $trailName, $location, $accessible, $length,$difficulty, $otherDets)
+          // rightDiv.append($ul);
+          console.log($(this).text());
+        })
+
+          $('.si-locat li').off().on('click',function(event) {
+          // event.preventDefault();
+          const $trail = $(this);
+          let $parkName=  $("<li>").text("Park: " + $trail.attr('data-park-name')).addClass('li-color');
+          let $trailName=  $("<li>").text("Trail Name: " + $trail.attr('data-trail-name')).addClass('li-color');
+          let $location=  $("<li>").text("Location: " + $trail.attr('data-location')).addClass('li-color');
+          let $accessible=  $("<li>").text("Accessible: " + $trail.attr('data-accessible')).addClass('li-color');
+          let $length=  $("<li>").text("Length: " + $trail.attr('data-length')).addClass('li-color');
+          let $difficulty=  $("<li>").text("Difficulty: " +$trail.attr('data-difficulty')).addClass('li-color');
+          let $otherDets = $("<li>").text("Details " + $trail.attr('data-other-details') ).addClass('li-color');
+
+          // APPENDING
+          let rightDiv = $('.right-align');
+          // let $ul = $(".brook-locat");
+          rightDiv.append($parkName, $trailName, $location, $accessible, $length,$difficulty, $otherDets)
+          // rightDiv.append($ul);
+          console.log($(this).text());
+        })
+
+          $('.queens-locat li').off().on('click',function(event) {
+          // event.preventDefault();
+          const $trail = $(this);
+          let $parkName=  $("<li>").text("Park: " + $trail.attr('data-park-name')).addClass('li-color');
+          let $trailName=  $("<li>").text("Trail Name: " + $trail.attr('data-trail-name')).addClass('li-color');
+          let $location=  $("<li>").text("Location: " + $trail.attr('data-location')).addClass('li-color');
+          let $accessible=  $("<li>").text("Accessible: " + $trail.attr('data-accessible')).addClass('li-color');
+          let $length=  $("<li>").text("Length: " + $trail.attr('data-length')).addClass('li-color');
+          let $difficulty=  $("<li>").text("Difficulty: " +$trail.attr('data-difficulty')).addClass('li-color');
+          let $otherDets = $("<li>").text("Details " + $trail.attr('data-other-details') ).addClass('li-color');
+
+          // APPENDING
+          let rightDiv = $('.right-align');
+          // let $ul = $(".brook-locat");
+          rightDiv.append($parkName, $trailName, $location, $accessible, $length,$difficulty, $otherDets)
+          // rightDiv.append($ul);
+          console.log($(this).text());
+        })
+
+        $('.manh-locat li').off().on('click',function(event) {
+          // event.preventDefault();
+          const $trail = $(this);
+          let $parkName=  $("<li>").text("Park: " + $trail.attr('data-park-name')).addClass('li-color');
+          let $trailName=  $("<li>").text("Trail Name: " + $trail.attr('data-trail-name')).addClass('li-color');
+          let $location=  $("<li>").text("Location: " + $trail.attr('data-location')).addClass('li-color');
+          let $accessible=  $("<li>").text("Accessible: " + $trail.attr('data-accessible')).addClass('li-color');
+          let $length=  $("<li>").text("Length: " + $trail.attr('data-length')).addClass('li-color');
+          let $difficulty=  $("<li>").text("Difficulty: " +$trail.attr('data-difficulty')).addClass('li-color');
+          let $otherDets = $("<li>").text("Details " + $trail.attr('data-other-details') ).addClass('li-color');
+
+          // APPENDING
+          let rightDiv = $('.right-align');
+          // let $ul = $(".brook-locat");
+          rightDiv.append($parkName, $trailName, $location, $accessible, $length,$difficulty, $otherDets)
+          // rightDiv.append($ul);
+          console.log($(this).text());
+        })
+
+        $('.brook-locat li').off().on('click',function(event) {
+          // event.preventDefault();
+          const $trail = $(this);
+          let $parkName=  $("<li>").text("Park: " + $trail.attr('data-park-name')).addClass('li-color');
+          let $trailName=  $("<li>").text("Trail Name: " + $trail.attr('data-trail-name')).addClass('li-color');
+          let $location=  $("<li>").text("Location: " + $trail.attr('data-location')).addClass('li-color');
+          let $accessible=  $("<li>").text("Accessible: " + $trail.attr('data-accessible')).addClass('li-color');
+          let $length=  $("<li>").text("Length: " + $trail.attr('data-length')).addClass('li-color');
+          let $difficulty=  $("<li>").text("Difficulty: " +$trail.attr('data-difficulty')).addClass('li-color');
+          let $otherDets = $("<li>").text("Details " + $trail.attr('data-other-details') ).addClass('li-color');
+
+          // APPENDING
+          let rightDiv = $('.right-align');
+          // let $ul = $(".brook-locat");
+          rightDiv.append($parkName, $trailName, $location, $accessible, $length,$difficulty, $otherDets)
+          // rightDiv.append($ul);
+          console.log($(this).text());
+        })
+
+
+
+
       }, fail: function(err) {
         console.log('conncection failed!');
           }
       });
+
 
 
 
