4fac5c2d7b54ce93a3a9bdfacddbd46eb3504caa
Luis Lithgow <lalithgow@gmail.com>
2016-08-31 14:10:24 -0400

    commit

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: false}) );
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('bower_components', express.static(path.join(__dirname, '/bower_components')));
+// app.use('bower_components', express.static(path.join(__dirname, '/bower_components')));
 app.use(methodOverride('_method'));
 
 // Setting up the routes
